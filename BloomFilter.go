48ad8874ad012c306174b7457a0f64351b780875
mhsso <mhs.sonic13@gmail.com>
2023-04-17 17:36:40 +0330

    clean up

--- BloomFilter.go
+++ BloomFilter.go
@@ -1,7 +1,6 @@
-package main
+package BloomFilter
 
 import (
-	"fmt"
 	"hash/fnv"
 )
 
@@ -9,30 +8,14 @@ const m uint64 = 350 * 1e7
 
 var bitArray = make([]bool, m)
 
-func main() {
-	added := []string{"hey", "ali", "pashmaki", "sfds", "sdidd", "reza", "hi", "dude", "nun", "tof"}
-	notAdded := []string{"hy", "hadi", "shokolat", "wqeds", "hgfde", "hasan", "james", "uwuwu", "12312f", "12edw"}
-	for _, str := range added {
-		addString(str)
-	}
-	for _, str := range added {
-		fmt.Printf("%s %t\n", str, BloomFilterIsThere(str))
-	}
-	fmt.Printf("-------------------\n")
-	for _, str := range notAdded {
-		fmt.Printf("%s %t\n", str, BloomFilterIsThere(str))
-	}
-
-}
-
-func addString(str string) {
+func AddString(str string) {
 	i, j, k := hash(str)
 	bitArray[i] = true
 	bitArray[j] = true
 	bitArray[k] = true
 }
 
-func BloomFilterIsThere(str string) bool {
+func IsThere(str string) bool {
 	i, j, k := hash(str)
 	return bitArray[i] && bitArray[j] && bitArray[k]
 }
